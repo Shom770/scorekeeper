08ea0f6994c5350bcfc5b2930069d848322df44b
Shom770 <shayaanwadkar@gmail.com>
2024-07-18 17:21:12 -0400

    working mvp

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request
-from utils import add_scores_to_database
+from utils import add_scores_to_database, stats_of_doubles_teams
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", type_of_game=request.args.get("type", "doubles"))
+    type_of_game = request.args.get("type") or "doubles"
+    section = request.args.get("section", "report_scores")
+
+    if section == "report_scores":
+        return render_template("report_scores.html", type_of_game=type_of_game)
+    elif section == "doubles_stats":
+        return render_template(
+            "doubles_stats.html",
+            doubles_stats=enumerate(stats_of_doubles_teams().items(), start=1),
+            places_to_colors={1: "yellow-600", 2: "stone-400", 3: "amber-800"}
+        )
+    else:
+        return render_template("personal_stats.html")
 
 
 @app.route("/process_scores", methods=["POST"])
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from os import environ
 
 from dotenv import load_dotenv
@@ -18,19 +19,48 @@ def add_scores_to_database(
     team_two_score: int
 ) -> None:
     """Adds the scores for each player accordingly to the database."""
+    team_one_players = sorted(name.lower() for name in team_one_players)
+    team_two_players = sorted(name.lower() for name in team_two_players)
+
     scores = database["scores"]
+    players = database["players"]
+
     winners = team_one_players if team_one_score > team_two_score else team_two_players
+    current_players = players.find()[0]["all_players"]
 
-    for player in team_one_players:
-        scores.insert_one({
-            "name": player.lower(),
-            "score": team_one_score,
-            "won_game": player in winners
-        })
+    # Add new players to the list if they exist
+    players.update_one(
+        {"all_players": current_players},
+        {"$set": {"all_players": list(set(current_players).union(team_one_players + team_two_players))}}
+    )
 
-    for player in team_two_players:
-        scores.insert_one({
-            "name": player.lower(),
-            "score": team_two_score,
-            "won_game": player in winners
-        })
+    # Insert scores
+    scores.insert_one({
+        "winners": {
+            "names": winners,
+            "score": max(team_one_score, team_two_score)
+        },
+        "losers": {
+            "names": team_one_players if team_two_players == winners else team_two_players,
+            "score": min(team_one_score, team_two_score)
+        }
+    })
+
+
+def stats_of_doubles_teams() -> dict:
+    """Retrieves the stats of all teams that have played."""
+    scores = database["scores"]
+    records = defaultdict(lambda: {"wins": 0, "losses": 0, "total_points": 0})
+
+    for game in list(scores.find()):
+        if len(game["winners"]["names"]) != 2 and len(game["losers"]["names"]) != 2:
+            continue
+
+        records[tuple(game["winners"]["names"])]["wins"] += 1
+        records[tuple(game["winners"]["names"])]["total_points"] += game["winners"]["score"]
+
+        records[tuple(game["losers"]["names"])]["losses"] += 1
+        records[tuple(game["losers"]["names"])]["total_points"] += game["losers"]["score"]
+
+    records = dict(sorted(records.items(), key=lambda pair: pair[1]["total_points"], reverse=True))
+    return {(key[0].title(), key[1].title()): value for key, value in records.items()}
